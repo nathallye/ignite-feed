34c4fbed7f5ebda8140f51139176482e7df763f3
nathallyet <nathallyet@gmail.com>
2023-02-07 00:58:21 -0300

    Criação do componente Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import Header from "./components/Header";
+import Post from "./components/Post";
 
 export default function App() {
 
   return (
     <div>
       <Header />
+      
+      <Post
+        author="Diego Fernandes"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+      />
+      <Post
+        author="Nathallye Bacelar"
+        content="Um novo post muito legal"
+      />
     </div>
   )
 }
